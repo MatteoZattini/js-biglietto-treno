b621bf8445e7a58ff8b5c13b769109ccf82db4a6
MatteoZattini <matteozattini@hotmail.com>
2024-04-17 17:32:40 +0200

    added prompt for age and km

---
+++ main.js
@@ -0,0 +1,11 @@
+const tariffa = (0.21)
+
+let UserAge = prompt ("inserisci la tua età")
+
+let UserKm = prompt ("Inserisci km")
+
+const Prezzo = ( UserKm * tariffa)
+
+if (UserAge < 18) {
+    
+}

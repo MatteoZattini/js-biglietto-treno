6f5d7634f0e4df6200d9e64da9ba042afb0242ea
MatteoZattini <matteozattini@hotmail.com>
2024-04-17 18:46:39 +0200

    added completed exercise

--- main.js
+++ main.js
@@ -4,8 +4,19 @@ let UserAge = prompt ("inserisci la tua età")
 
 let UserKm = prompt ("Inserisci km")
 
-const Prezzo = ( UserKm * tariffa)
+let Prezzo = ( UserKm * tariffa)
+
+const ScontoJunior = (Prezzo * 20 / 100)
+
+const ScontoSenior = (Prezzo * 40 / 100)
 
 if (UserAge < 18) {
-    
+    console.log(Prezzo - ScontoJunior)
+    document.getElementById("Stampa").innerHTML= Prezzo.toFixed(1) - ScontoJunior.toFixed(1)
+} else if (UserAge > 65) {
+    console.log(Prezzo - ScontoSenior)
+    document.getElementById("Stampa").innerHTML= Prezzo.toFixed(1) - ScontoSenior.toFixed(1)
+} else if (UserAge >= 18 && UserAge < 65) {
+    console.log (Prezzo)
+    document.getElementById("Stampa").innerHTML= Prezzo.toFixed(1)
 }
